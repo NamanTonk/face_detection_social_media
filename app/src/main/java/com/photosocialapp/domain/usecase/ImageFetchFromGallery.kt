950247230006fbf04e9b11ab760180f051ed52b4
namantonk <namantonk@gmail.com>
2025-09-28 13:58:01 +0530

    apply limit on content resolver

--- app/src/main/java/com/photosocialapp/domain/usecase/ImageFetchFromGallery.kt
+++ app/src/main/java/com/photosocialapp/domain/usecase/ImageFetchFromGallery.kt
@@ -1,20 +1,29 @@
 package com.photosocialapp.domain.usecase
 
+import android.content.ContentResolver
 import android.content.Context
+import android.os.Build
+import android.os.Bundle
 import android.provider.MediaStore
 
 class ImageFetchFromGallery(private val context: Context) {
     /**
      * Queries the MediaStore for the latest 50 images sorted by date added
      */
-    operator fun invoke() = context.contentResolver.query(
-        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-        arrayOf(
-            MediaStore.Images.Media._ID,
-            MediaStore.Images.Media.DATE_ADDED
-        ),
-        null,
-        null,
-        "${MediaStore.Images.Media.DATE_ADDED} DESC LIMIT 50"
-    )
+    operator fun invoke(page: Int = 0)  = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        context.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DATE_ADDED
+            ),
+
+            Bundle().apply {
+                putString(ContentResolver.QUERY_ARG_SORT_COLUMNS, MediaStore.Images.Media.DATE_ADDED)
+                putInt(ContentResolver.QUERY_ARG_SORT_DIRECTION, ContentResolver.QUERY_SORT_DIRECTION_DESCENDING)
+                putInt(ContentResolver.QUERY_ARG_LIMIT, 50)
+                putInt(ContentResolver.QUERY_ARG_OFFSET, page * 50) // page = 0,1,2...
+            },null
+        )
+    }else throw Exception("SDK version should be greater than 26")
 }
